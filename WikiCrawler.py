bb569d3a188fc7b93ff390f2e0e91b543f6ccecf
Manjeet <manjeet.nsit@gmail.com>
2018-01-12 19:09:03 +0530

    Added Crawling Code

--- WikiCrawler.py
+++ WikiCrawler.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import time
+import pprint
 
 
 def check_crawler(links_list, new_link, target_url):
     if new_link in links_list[:-1]:
         print ("Link directed to earlier link in the search history. Aborting")
         return False
-    elif len(links_list)>25:
+    elif len(links_list)>35:
         print ("No. of search trials exceeded limit. Aborting search")
         return  False
     elif new_link == target_url:
@@ -22,5 +23,27 @@ def check_crawler(links_list, new_link, target_url):
 
 target_url = "https://en.wikipedia.org/wiki/Philosophy"
 start_url = "https://en.wikipedia.org/wiki/Animal"
+link_url = start_url
+link_lst = []
+link_lst.append(start_url)
+itr = 0
+
+while(check_crawler(link_lst,link_url,target_url)):
+    response = requests.get(link_url)
+    soup = bs(response.content, 'lxml')
+    lst = soup.find_all('p')
+    try:
+        childList = lst[0].findChildren()
+        for child in childList:
+
+        link_url = "https://en.wikipedia.org" + lst[0].findChild('a')['href']
+    except:
+        link_url = None
+    link_lst.append(link_url)
+    itr +=1
+    print (itr)
+    #time.sleep(0.5)
+
+pprint.pprint(link_lst)
 
 
